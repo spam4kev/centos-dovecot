13b58b858e596b3764f3a4340b4521879e7780dd
kev kev <spam4kev@gmail.com>
2016-02-06 10:38:42 -0800

    firewall settings

--- userData.sh
+++ userData.sh
@@ -8,9 +8,9 @@ sed -i 's/#mail_location =/mail_location = mbox:~\/mail:INBOX=\/var\/mail\/\%u/'
 sed -i 's/#mail_privileged_group =/mail_privileged_group = mail/' /etc/dovecot/conf.d/10-mail.conf
 sed -i 's/auth_mechanisms = plain/auth_mechanisms = plain login/' /etc/dovecot/conf.d/10-auth.conf
 sed -i '/unix_listener \/var\/spool\/postfix\/private\/auth/,/  #\}/{s/#/ /g;}' /etc/dovecot/conf.d/10-master.conf
-sed -i 's/#mydomain = domain.tld/mydomain = fitznet.local/' /etc/postfix/main.cf
+sed -i 's/#mydomain = domain.tld/mydomain = ddns.net/' /etc/postfix/main.cf
 sed -i 's/#myorigin/myorigin/' /etc/postfix/main.cf
-sed -i 's/#myhostname = host.domain.tld/myhostname = dovecot.fitznet.local/' /etc/postfix/main.cf
+sed -i 's/#myhostname = host.domain.tld/myhostname = thefitz1.ddns.net/' /etc/postfix/main.cf
 sed -i 's/#mynetworks = 168.100.189.0\/28/mynetworks = 10.11.11.0\/24/' /etc/postfix/main.cf
 sed -i 's/inet_interfaces = localhost/inet_interfaces = all/' /etc/postfix/main.cf
 echo "0.0.0.0 OK" >> /etc/postfix/access
