537c63698d11321dda9a74aafac653f0a4906c99
kev kev <spam4kev@gmail.com>
2016-02-05 19:46:32 -0800

    wip

--- userData.sh
+++ userData.sh
@@ -38,6 +38,7 @@ firewall-cmd --permanent --add-service=smtp
 firewall-cmd --permanent --add-service=ldap
 firewall-cmd --permanent --add-service=ldaps
 #firewall-cmd --permanent --add-service=imap
+firewall-cmd --permanent --add-port=143/tcp
 firewall-cmd --permanent --add-service=imaps
 systemctl enable dovecot.service
 systemctl enable postfix.service
