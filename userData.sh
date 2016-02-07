6095ae997ff60c3400c0f2d7f6a56d43294ed241
kev kev <spam4kev@gmail.com>
2016-02-07 09:17:36 -0800

    cleanup

--- userData.sh
+++ userData.sh
@@ -14,11 +14,14 @@ sed -i 's/#myorigin = $mydomain/myorigin = $mydomain/' /etc/postfix/main.cf
 sed -i 's/#myhostname = host.domain.tld/myhostname = thefitz1.ddns.net/' /etc/postfix/main.cf
 sed -i 's/#mynetworks = 168.100.189.0\/28/mynetworks = 0.0.0.0\/0/' /etc/postfix/main.cf
 sed -i 's/inet_interfaces = localhost/inet_interfaces = all/' /etc/postfix/main.cf
-sed -i 's/#relayhost = \[gateway.my.domain\]/relayhost = \[smtp.verizon.net\]/' /etc/postfix/main.cf
+sed -i 's/#relayhost = \[gateway.my.domain\]/relayhost = smtp.verizon.net:587/' /etc/postfix/main.cf
 echo "0.0.0.0 OK" >> /etc/postfix/access
+echo "smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd" >> /etc/postfix/main.cf
+echo "smtpd_use_tls = yes" >> /etc/postfix/main.cf
 echo "smtpd_sasl_type = dovecot" >> /etc/postfix/main.cf
 echo "smtpd_sasl_path = private/auth" >> /etc/postfix/main.cf
-echo "smtpd_sasl_auth_enable = yes" >> /etc/postfix/main.cf
+echo "smtp_sasl_auth_enable = yes" >> /etc/postfix/main.cf
+echo "smtp_sasl_security_options = noanonymous" >> /etc/postfix/main.cf
 echo "broken_sasl_auth_clients = yes" >> /etc/postfix/main.cf
 echo "smtpd_recipient_restrictions =" >> /etc/postfix/main.cf
 echo "   permit_mynetworks" >> /etc/postfix/main.cf
@@ -32,7 +35,7 @@ mv /etc/pki/dovecot/private/dovecot.pem /tmp
 touch /etc/ssl/certs/smtpd.key
 chmod 600 /etc/ssl/certs/smtpd.key
 openssl genrsa 1024 > /etc/ssl/certs/smtpd.key
-openssl req -new -key /etc/ssl/certs/smtpd.key -x509 -days 3650 -config /etc/pki/dovecot/dovecot-openssl.cnf -out /etc/ssl/certs/smtpd.key
+openssl req -new -key /etc/ssl/certs/smtpd.key -x509 -days 3650 -config /etc/pki/dovecot/dovecot-openssl.cnf -out /etc/ssl/certs/smtpd.crt
 echo 'mailbox_command = /usr/libexec/dovecot/dovecot-lda -f "$SENDER" -a "$RECIPIENT"' >> /etc/postfix/main.cf
 echo "root:           spam4kev" >> /etc/aliases
 newaliases
@@ -41,10 +44,9 @@ firewall-offline-cmd --add-service=ldap
 firewall-offline-cmd --add-service=ldaps
 firewall-offline-cmd --add-port=143/tcp
 firewall-offline-cmd --add-service=imaps
-#firewall-cmd --reload
-#echo "smtp.verizon.net spam4kev-dovecot:$(openssl rand -base64 32)" > /etc/postfix/sasl_passwd
-#postmap hash:/etc/postfix/sasl_passwd 
-#chmod 600 /etc/postfix/sasl_passwd.db /etc/postfix/sasl_passwd
+echo "smtp.verizon.net:587 dovecot1:$(openssl rand -base64 32)" > /etc/postfix/sasl_passwd
+postmap hash:/etc/postfix/sasl_passwd 
+chmod 600 /etc/postfix/sasl_passwd.db /etc/postfix/sasl_passwd
 systemctl enable dovecot.service
 systemctl enable postfix.service
 systemctl start postfix.service
